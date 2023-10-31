7fac7053340b2b0ff08fedeaec945ffa83a53bd7
kloepoitrot <KloePoitrot@hotmail.com>
2023-10-31 11:12:16 +0100

    Modification du panier
    Possibilité de modifier ou supprimer les items

--- front/js/displayCart.js
+++ front/js/displayCart.js
@@ -20,7 +20,6 @@ async function displayCart(){
     let finalPrice = 0
     let finalQuant = 0
     
-    // affiche si il y a au moin 1 item
     if(cart !== null){
         cart.forEach(product => {
             api = datas.find(el => el._id == product.id)
@@ -41,7 +40,7 @@ async function displayCart(){
                             <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantite}">
                         </div>
                         <div class="cart__item__content__settings__delete">
-                            <p class="deleteItem">Supprimer</p>
+                            <p data-id="${product.id}" data-color="${product.couleur}" class="deleteItem">Supprimer</p>
                         </div>
                         </div>
                     </div>
@@ -49,12 +48,9 @@ async function displayCart(){
             `
             finalPrice += parseInt(product.quantite) * api.price
             finalQuant += parseInt(product.quantite)
-    
-            console.log(finalPrice, finalQuant)
         });
     }
 
-    // Affiche si il n'y a aucun item
     if(cart == null){
         addInfo = '<h2>Votre panier est vide</h2>'
     }
@@ -63,4 +59,66 @@ async function displayCart(){
     document.querySelector('#cart__items').innerHTML = addInfo
     document.querySelector('#totalQuantity').innerHTML = finalQuant
     document.querySelector('#totalPrice').innerHTML = finalPrice
+
+
+
+
+    // Change la quantité d'un item
+    let articleQuant = document.querySelectorAll('article')
+    articleQuant.forEach(el => (
+        el.addEventListener('change', (e) => {
+            changeQuant(e, cart)
+        })
+    ))
+
+    // Supprime un item
+    let articleDelete = document.querySelectorAll('.deleteItem')
+    articleDelete.forEach(el => (
+        el.addEventListener('click', (e) => {
+            console.log('wee')
+            deleteItem(e, cart)
+        })
+    ))
+
 }
+
+
+
+
+// Fonction change quantité
+function changeQuant(el, cart){
+    let id = el.currentTarget.dataset.id
+    let color = el.currentTarget.dataset.color
+    let quant = el.target.value
+    let quantCart = cart.find(product => product.id == id && product.couleur == color)
+    
+    quantCart.quantite = parseInt(quant)
+
+    if(quantCart.quantite > 100){
+        quantCart.quantite = 100
+        el.target.value = 100
+    }
+    if(quantCart.quantite <= 0){
+        quantCart.quantite = 1
+        el.target.value = 1
+    }
+    
+    localStorage.setItem("cart", JSON.stringify(cart))
+    displayProducts()
+}
+
+
+
+
+// Fonction Supprimer item
+function deleteItem(el, cart){
+    let id = el.currentTarget.dataset.id
+    let color = el.currentTarget.dataset.color
+    let delCart = cart.filter(product => product.id !== id || product.couleur !== color)
+    
+
+    localStorage.setItem("cart", JSON.stringify(delCart))
+    displayProducts()
+}
+
+
